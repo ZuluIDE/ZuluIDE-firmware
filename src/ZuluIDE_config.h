f1627d339e2aba449af6dd7e143bcac4eba04069
Alex Perez <aperez@rabbithole.ws>
2023-07-06 10:05:39 -0700

    Update ZuluIDE_config.h

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -6,7 +6,7 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2023.06.29"
+#define FW_VER_NUM      "2023.07.06"
 #define FW_VER_SUFFIX   "dev"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
