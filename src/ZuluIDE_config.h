eaf4367a9494a9fe10ab6cbb871c8fdb1551a4e8
Alex Perez <aperez@rabbithole.ws>
2024-01-30 09:41:20 -0800

    Iterate version string

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -27,7 +27,7 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2024.01.08"
+#define FW_VER_NUM      "2024.01.30"
 #define FW_VER_SUFFIX   "dev"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
