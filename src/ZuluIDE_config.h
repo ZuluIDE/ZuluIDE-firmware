a37d6058e9dd21e5430e3746c67d20acd6d8d248
Alex Perez <aperez@rabbithole.ws>
2024-11-08 14:05:33 -0800

    Prepare for v2024.11.08 release

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -27,8 +27,8 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2024.11.07"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_NUM      "2024.11.08"
+#define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
