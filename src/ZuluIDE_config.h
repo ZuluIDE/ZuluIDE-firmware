3824b0014da9eb1a29ebefbd8fc89af488d4d649
Alex Perez <aperez@rabbithole.ws>
2025-01-02 14:53:44 -0800

    Update ZuluIDE_config.h

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -27,7 +27,7 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2024.12.12"
+#define FW_VER_NUM      "2025.01.01"
 #define FW_VER_SUFFIX   "devel"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
