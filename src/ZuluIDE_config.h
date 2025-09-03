322d41302c4b36cc18117324967688544844cee3
Alex Perez <aperez@rabbithole.ws>
2025-09-03 13:19:39 -0700

    Prepare for beta2 release

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -31,8 +31,8 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2025.08.13"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2025.09.03"
+#define FW_VER_SUFFIX   "beta2"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
