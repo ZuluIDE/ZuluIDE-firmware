b95c13ce68e542a3a102ffd1e46772a806d54a33
Alex Perez <aperez@rabbithole.ws>
2025-12-30 10:56:16 -0800

    Prepare for v2025.12.30 release

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -31,7 +31,7 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2025.12.27"
+#define FW_VER_NUM      "2025.12.30"
 #define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
