3b69d2fa3f8b94893dc5a26a6ad2ab03977569de
Alex Perez <aperez@rabbithole.ws>
2026-02-11 11:03:33 -0800

    Prepare for v2026.02.11 bugfix release (V2 specific)

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -31,7 +31,7 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.02.02"
+#define FW_VER_NUM      "2026.02.11"
 #define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
