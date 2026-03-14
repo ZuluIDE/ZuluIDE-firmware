8f8bc6af6f502f46488f90cfde1dd7bda5c0ae82
Alex Perez <aperez@rabbithole.ws>
2026-03-13 17:00:40 -0700

    Prepare for v2026.03.14 release

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -31,8 +31,8 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.03.12"
-#define FW_VER_SUFFIX   "devel"
+#define FW_VER_NUM      "2026.03.14"
+#define FW_VER_SUFFIX   "release"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
