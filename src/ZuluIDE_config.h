bcbf9ce19c1e71fe6bea35e8febb926c1690a2db
Alex Perez <aperez@rabbithole.ws>
2026-05-19 10:34:23 -0700

    iterate dev version string

--- src/ZuluIDE_config.h
+++ src/ZuluIDE_config.h
@@ -31,8 +31,8 @@
 #include <ZuluIDE_platform.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.04.27"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2026.05.19"
+#define FW_VER_SUFFIX   "dev"
 #define ZULU_FW_VERSION FW_VER_NUM "-" FW_VER_SUFFIX
 
 // Configuration and log file paths
